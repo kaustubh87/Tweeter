efe66a4340dc06c849ed583b974cd8166b118947
kvin1087 <kaustubh87@gmail.com>
2017-04-27 16:32:50 -0500

    First Commit

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
+var session = require('express-session');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+
 
 var api = require('./routes/api');
-//var authenticate = require('./routes/authenticate');
+var authenticate = require('./routes/authenticate')(passport);
 
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -17,13 +19,23 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(session({
+    secret : 'Super secret'
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use('/', index);
-app.use('/users', users);
+//Initialize passport
+
+var initPassport = require('./passport-init');
+initPassport(passport);
+
+app.use('/api', api);
+app.use('/auth', authenticate);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/api.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+
+//  Some implementation....
+
+router.route('/posts')
+.get(function(req,res){
+   
+    res.send({message: 'TODO return all posts'});
+
+})
+.post(function(req,res){
+   
+    res.send({message: 'TODO Create a new post'});
+});
+
+router.route('/posts/:id')
+.get(function(req,res){
+   
+    res.send({message: 'TODO return post with id ' +req.params.id});
+    
+})
+.put(function(req,res){
+   
+    res.send({message : 'TODO modify post with ID ' +req.params.id});
+})
+.delete(function(req,res){
+   
+    res.send({message : 'TODO delete post with ID ' +req.params.id});
+});
+
+
+module.exports = router;
