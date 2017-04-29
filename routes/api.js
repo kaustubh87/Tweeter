a6c502ec16386b090d8f568981c9ddce017fc28b
kvin1087 <kaustubh87@gmail.com>
2017-04-29 14:46:03 -0500

    Added new get route

--- routes/api.js
+++ routes/api.js
@@ -1,23 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
-router.use(function(req,res,next){
-   
-    if(req.method === "GET"){
-        
-        return next();
-    }
-       
-    
-    if(!req.isAuthenticated()){
-       
-       //user not authenticated redirect to login page
-        res.redirect('/#login');   
-    }
-    
-});
+//Used for routes that must be authenticated.
+function isAuthenticated (req, res, next) {
+	// if user is authenticated in the session, call the next() to call the next request handler 
+	// Passport adds this method to request object. A middleware is allowed to add properties to
+	// request and response objects
 
-//  Some implementation....
+	//allow all get request methods
+	if(req.method === "GET"){
+		return next();
+	}
+	if (req.isAuthenticated()){
+		return next();
+	}
+
+	// if the user is not authenticated then redirect him to the login page
+	return res.redirect('/#login');
+};
+
+//Register the authentication middleware
+router.use('/posts', isAuthenticated);
+
+//router.use('/posts');
+
+router.route('/posts/:id')
+.get(function(req,res){
+   console.log(req.params.id); 
+});
 
 router.route('/posts')
 .get(function(req,res){
